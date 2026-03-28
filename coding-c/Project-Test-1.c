c1e88cf3289a10596854cc3e927ffe3b236d45c4
xAtegox <thegamingrene@gmail.com>
2026-03-28 22:00:56 +0100

    peak

---
+++ coding-c/Project-Test-1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    // Print Message
+    printf("this is an tutorial for output!\n");
+
+    // Variables
+
+    // Int is whole number only
+    int age = 25;
+    int year = 2026;
+    int quantity =2;
+
+    printf("You are %d Years old\n", age);
+    printf("The year is %d\n", year);
+    printf("You have ordered %d x items\n", quantity);
+
+    // Float is decimal numbers
+    float gpa = 2.5;
+    float price =19.99;
+
+    printf("your gpa is %f\n", gpa);
+    printf("The price is $%.2f\n", price);
+    // %.2f means 2 decimals .3 would mean 3....
+
+
+
+    return 0;
+}
